3966686524151f6e4602bb4234ef6aedc67844f1
Domenic Anteja <antejadomenic123@gmail.com>
2024-03-13 19:55:29 +0800

    Added top of the side part and added img

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,59 +1,84 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Profile from "./Profile/Profile";
+import Notification from "./Profile/Notification";
+import UserMenuButton from "./Profile/UserMenuButton";
+import StudentDashboardItem from "./Profile/StudentDashboardItem";
+import home from "./Profile/images/home.png";
+import STCLARE from "./Profile/images/STCLARE.png"
+import course from "./Profile/images/course.png"
+import profile from "./Profile/images/profile.png"
+import grades from "./Profile/images/grades.png"
+import report from "./Profile/images/report.png"
+import settings from "./Profile/images/settings.png"
+
 const Sidebar = () => {
   return (
-    <div className="bg-red-900 text-white h-screen w-64 flex flex-col justify-between">
+    <div className="bg-red-900 text-white h-screen w-64 flex flex-col justify-between "> 
       <div className="p-4">
+        <div className="flex items-center w-full p-4 pl-6">
+          <div className="flex items-center justify-center">
+            <img className="w-8" src={STCLARE}/>
+          </div>
+            <div className="flex items-center ml-auto">
+              <Notification>
+              </Notification>
+              <UserMenuButton>
+              </UserMenuButton>
+            </div>
+        </div>
         <Profile
           imageUrl="url_to_your_image.jpg"
           name="Example name"
           gmail="example@gmail.com"
         />
+        <div className="-ml-14">
+        <StudentDashboardItem/>
+        </div>
         <ul className="space-y-2">
           <li>
-            <Link to="/" className="block hover:text-gray-300">
-              Home
+            <Link to="/" className="hover:text-gray-300 bg-red rounded-lg shadow-md p-2  flex items-center">
+              <img className="w-7 mr-8"src={home}/>Home
             </Link>
           </li>
           <li>
-            <Link to="/courses" className="block hover:text-gray-300">
-              Courses
+            <Link to="/courses" className=" hover:text-gray-300 bg-red rounded-lg shadow-md p-2 flex items-center">
+              <img className="w-7 mr-8"src={course}/>Courses
             </Link>
           </li>
           <li>
-            <Link to="/students" className="block hover:text-gray-300">
-              Students
+            <Link to="/students" className=" hover:text-gray-300 bg-red rounded-lg shadow-md p-2 flex items-center">
+              <img className="w-7 mr-8"src={profile}/>Students
             </Link>
           </li>
           <li>
-            <Link to="/grades" className="block hover:text-gray-300">
-              Grades
+            <Link to="/grades" className=" hover:text-gray-300 bg-red rounded-lg shadow-md p-2 flex items-center">
+              <img className="w-7 mr-8"src={grades}/>Grades
             </Link>
           </li>
           <li>
-            <Link to="/reports" className="block hover:text-gray-300">
-              Reports
+            <Link to="/reports" className=" hover:text-gray-300 bg-red rounded-lg shadow-md p-2 flex items-center">
+              <img className="w-7 mr-8"src={report}/>Reports
             </Link>
           </li>
           <li>
-            <Link to="/settings" className="block hover:text-gray-300">
-              Settings
+            <Link to="/settings" className=" hover:text-gray-300 bg-red rounded-lg shadow-md p-2 flex items-center">
+              <img className="w-7 mr-8"src={settings}/>Settings
             </Link>
           </li>
         </ul>
       </div>
       <div className="p-4">
         <div>
-          <Link to="/logs" className="block hover:text-red-800">
+          <Link to="/logs" className="block hover:text-red-800 bg-red rounded-lg shadow-md p-4">
             Logs
           </Link>
         </div>
         <p className="text-sm">
           Your Company Name &copy; 2024. All rights reserved.
         </p>
-      </div>
-    </div>
+        </div>
+  </div>
   );
 };
 
